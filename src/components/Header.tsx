2d1a7709fd7f7999efbf472af74838dca309af81
minu1123 <miszaelnunescosta@gmail.com>
2025-05-31 18:55:10 -0300

    resolv bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,22 @@ const Header: React.FC = () => {
   return (
     <header className="main-header">
       <div className="container">
-      <a href="#inicio"><div className="logo">Enervape</div></a>  
+        <a href="#inicio"><div className="logo">Enervape</div></a>
+
+        {/* Checkbox invisível para controlar o menu */}
+        <input type="checkbox" id="menu-toggle" />
+
+        {/* Label que funciona como botão de hambúrguer */}
+        <label htmlFor="menu-toggle" className="menu-icon">☰</label>
+
+        {/* Menu de navegação responsivo */}
         <nav className="nav-links">
           <a href="#mission">Missão</a>
           <a href="#about">Sobre</a>
           <a href="#usecases">Casos de uso</a>
           <a href="#footer">Enervape</a>
+          <a href="#contato" className="header-button">Fale Conosco</a>
         </nav>
-        <a href="#contato" className="header-button">Fale Conosco</a>
       </div>
     </header>
   );
