41d7f06d251fda4cd51bece9eca841481112e170
minu1123 <miszaelnunescosta@gmail.com>
2025-05-31 19:12:21 -0300

    teste2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,16 @@
-import React from 'react';
-import './Header.css';
+<header className="main-header">
+  <div className="container">
+    <a href="#inicio" className="logo">Enervape</a>
 
-const Header: React.FC = () => {
-  return (
-    <header className="main-header">
-      <div className="container">
-        <a href="#inicio" className="logo">Enervape</a>
+    <input type="checkbox" id="menu-toggle" />
+    <label htmlFor="menu-toggle" className="menu-icon">☰</label>
 
-        <input type="checkbox" id="menu-toggle" />
-        <label htmlFor="menu-toggle" className="menu-icon">☰</label>
-
-        <div className="menu-wrapper">
-          <nav className="nav-links">
-            <a href="#mission">Missão</a>
-            <a href="#about">Sobre</a>
-            <a href="#usecases">Casos de uso</a>
-            <a href="#footer">Enervape</a>
-            <a href="#contato" className="header-button">Fale Conosco</a>
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+    <nav className="nav-links">
+      <a href="#mission">Missão</a>
+      <a href="#about">Sobre</a>
+      <a href="#usecases">Casos de uso</a>
+      <a href="#footer">Enervape</a>
+      <a href="#contato" className="header-button">Fale Conosco</a>
+    </nav>
+  </div>
+</header>
