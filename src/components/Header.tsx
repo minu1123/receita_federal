993d8d6dc99aaf58445e03c29e0a30902a463ba4
minu1123 <miszaelnunescosta@gmail.com>
2025-05-31 19:20:22 -0300

    teste

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,29 @@
-<header className="main-header">
-  <div className="container">
-    <div className="header-left">
-      <a href="#inicio" className="logo">Enervape</a>
-    </div>
+import React from 'react';
+import './Header.css';
 
-    <div className="header-center">
-      <nav className="nav-links">
-        <a href="#mission">Missão</a>
-        <a href="#about">Sobre</a>
-        <a href="#usecases">Casos de uso</a>
-        <a href="#footer">Enervape</a>
-      </nav>
-    </div>
+const Header: React.FC = () => {
+  return (
+    <header className="main-header">
+      <div className="container">
+        <a href="#inicio"><div className="logo">Enervape</div></a>
 
-    <div className="header-right">
-      <a href="#contato" className="header-button">Fale Conosco</a>
-    </div>
-  </div>
-</header>
+        {/* Checkbox invisível para controlar o menu */}
+        <input type="checkbox" id="menu-toggle" />
+
+        {/* Label que funciona como botão de hambúrguer */}
+        <label htmlFor="menu-toggle" className="menu-icon">☰</label>
+
+        {/* Menu de navegação responsivo */}
+        <nav className="nav-links">
+          <a href="#mission">Missão</a>
+          <a href="#about">Sobre</a>
+          <a href="#usecases">Casos de uso</a>
+          <a href="#footer">Enervape</a>
+          <a href="#contato" className="header-button">Fale Conosco</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
